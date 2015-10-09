bb35bbd451d2811991396b7be7e18a531a33b2f5
Martin Jackson <mjackson@equalexperts.com>
2015-10-09 12:32:22 +0100

    Create example sinatra ruby application
    Create example sinatra ruby application that will expose a number of
    endpoints:
    
    * Home (/) - Homepage with information on exposed endpoints
    * Environment Variables (/environment) - Environment variables
    * Sleep (/sleep/INTEGER) - end point to delay the response to each
      call by a configurable number of milliseconds.

---
+++ sinatra-example.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'sinatra/base'
+
+class SinatraExample < Sinatra::Base
+  get '/' do
+    erb :home
+  end
+
+  get '/environment' do
+    request.env.map { |e| e.to_s + "\n" }
+  end
+
+  get '/sleep/:milliseconds' do
+    milliseconds = params[:milliseconds].to_i
+    sleep(milliseconds/1000)
+    "slept for #{milliseconds} milliseconds"
+  end
+
+end
