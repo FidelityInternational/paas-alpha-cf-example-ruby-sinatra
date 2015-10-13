bcbe7a9dba3c10b96b0397afd404250e38c33741
Hector Rivas Gandara <keymon@gmail.com>
2015-10-13 12:43:34 +0100

    Add endpoint to allocate portions of memory
    The endpoint `/mem/<size_mem_in_mb>/<leak>` will allocate a portion
    of memory. It receives the amount of megabytes to allocate and
    optionally if it should leak memory or not (1 enables leak)
    
    The endpoint returns some memory metrics for the process and global
    system memory.
    
    We use the gems `vmstat` and `os` to get system and process metrics
    across multiple platforms. To allocate the memory, we create a
    string of the given size.

--- sinatra-example.rb
+++ sinatra-example.rb
@@ -2,6 +2,8 @@
 
 require 'sinatra/base'
 require 'tilt/erb'
+require 'vmstat'
+require 'os'
 
 def genload
   1000.times do |i|
@@ -39,4 +41,41 @@ class SinatraExample < Sinatra::Base
     "Finished generating CPU load - #{processes} processes complete"
   end
 
+  get '/mem/:size_mb/?:leak?' do
+    content_type 'application/text'
+
+    size_mb = params[:size_mb].to_i
+    total_bytes = size_mb*1024**2
+    do_leak = (params[:leak] and params[:leak] == "1")
+
+    # Metrics before allowcate
+    rss_bytes_before = OS.rss_bytes
+    free_mem_before = (Vmstat.memory.free * Vmstat.memory.pagesize) / 1024
+
+    puts "Allocating: #{size_mb} MB. Leaking memory: #{if do_leak; "Yes" else  "No" end}, system free: #{free_mem_before/1024} MiB, Process rss: #{rss_bytes_before/1024} MiB"
+
+    # Allocate the memory chunk
+    chunk_of_mem = 'a'*(total_bytes-1)
+    if do_leak
+        $leak_buffer = [] unless $leak_buffer
+        $leak_buffer << chunk_of_mem
+    end
+
+    sleep(1.0/10.0) # To be sure that Vmstat will catch the memory change
+
+    # Metrics after allocate
+    rss_bytes_after = OS.rss_bytes
+    free_mem_after = (Vmstat.memory.free * Vmstat.memory.pagesize) / 1024
+
+    puts "Allocated: #{size_mb} MB. Leaking memory: #{if do_leak; "Yes" else  "No" end}, system free: #{free_mem_after/1024} MiB, Process rss: #{rss_bytes_after/1024} MiB"
+
+    """Allocated #{size_mb} MB = #{total_bytes} bytes.
+Leaking memory: #{if do_leak; "Yes" else  "No" end}
+Free system memory before: #{free_mem_before} kiB aka #{free_mem_before/1024} MiB
+Free system memory after: #{free_mem_after} kiB aka #{free_mem_after/1024} MiB
+Process memory before: #{rss_bytes_before} kiB aka #{rss_bytes_before/1024} MiB
+Process memory after: #{rss_bytes_after} kiB aka #{rss_bytes_after/1024} MiB
+"""
+
+  end
 end
