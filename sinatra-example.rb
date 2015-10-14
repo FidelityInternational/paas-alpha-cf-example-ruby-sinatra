d8de5a999b4e4d0d0d90ddb56716381532f014ca
Martin Jackson <mjackson@equalexperts.com>
2015-10-14 10:22:27 +0100

    Add cpuload endpoint to sample application

--- sinatra-example.rb
+++ sinatra-example.rb
@@ -2,6 +2,14 @@
 
 require 'sinatra/base'
 
+def genload
+  1000.times do |i|
+    100000.downto(1) do |j|
+      Math.sqrt(j) * i / 0.2
+    end
+  end
+end
+
 class SinatraExample < Sinatra::Base
   get '/' do
     erb :home
@@ -15,6 +23,16 @@ class SinatraExample < Sinatra::Base
     milliseconds = params[:milliseconds].to_i
     sleep(milliseconds/1000)
     "slept for #{milliseconds} milliseconds"
+  end
+
+  get '/cpuload' do
+    child_pid = Process.fork do
+      genload
+    end
+
+    Process.detach child_pid
+
+    "Generating CPU load in the background. Hit refresh to generate more load"
   end
 
 end
